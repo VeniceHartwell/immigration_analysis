aa2b338ba58b37ffca54d060b30ac7e082062ee0
Venice Hartwell <venicext@gmail.com>
2022-11-18 18:51:46 +0100

    primary queries complete

--- us_immigration.sql
+++ us_immigration.sql
@@ -14,17 +14,18 @@ CREATE TABLE visa (
     full_time bool,
     wage INT,
     year YEAR,
-    city VARCHAR(24),
+    city VARCHAR(32),
     state VARCHAR(20)
 );
 
 -- test; view sample of data
-SELECT * FROM visa
-LIMIT 10;
+SELECT COUNT(*) FROM visa;
+-- LIMIT 10;
 
 -- Most lucrative field, by population
-SELECT SUM(wage), COUNT(field) AS num_in_field, field
+SELECT SUM(wage) AS total_wage, COUNT(field) AS num_in_field, field
 FROM visa
+WHERE status = 'CERTIFIED-WITHDRAWN' OR status = 'CERTIFIED'
 GROUP BY field
 HAVING COUNT(field) > 2 -- filter out misc fields (usually mistypes)
 ORDER BY num_in_field DESC;
@@ -32,6 +33,7 @@ ORDER BY num_in_field DESC;
 -- Most lucrative field, by total wage
 SELECT SUM(wage) AS total_wage, COUNT(field) AS num_in_field, field
 FROM visa
+WHERE status = 'CERTIFIED-WITHDRAWN' OR status = 'CERTIFIED'
 GROUP BY field
 HAVING COUNT(field) > 2 -- filter out misc fields (usually mistypes)
 ORDER BY total_wage DESC;
@@ -39,6 +41,7 @@ ORDER BY total_wage DESC;
 -- Most lucrative state, by population
 SELECT SUM(wage) AS total_wage, COUNT(state) AS num_in_state, state
 FROM visa
+WHERE status = 'CERTIFIED-WITHDRAWN' OR status = 'CERTIFIED'
 GROUP BY state
 ORDER BY num_in_state DESC;
 
@@ -48,8 +51,25 @@ FROM visa
 GROUP BY state
 ORDER BY total_wage DESC;
 
+-- Visa petition growth YoY
+SELECT COUNT(year) AS total_petitions, year
+FROM visa
+GROUP BY year;
 
+-- Visa petitions by employment status
+SELECT COUNT(full_time) as total_petitions, CASE 
+	WHEN full_time = 1 
+	THEN 'Y' ELSE 'N' 
+END AS full_time
+FROM visa
+GROUP BY full_time;
 
+-- OTHER QUERY FUNCTIONS I WOULD LIKE TO FIND A REASON TO USE:
+
+-- EXISTS https://www.codewars.com/kata/58113a64e10b53ec36000293
+-- POSITION https://www.codewars.com/kata/59401e0e54a655a298000040
+-- JOIN and RANK https://www.codewars.com/kata/58094559c47d323ebd000035
+-- UNION ALL https://www.codewars.com/kata/58112f8004adbbdb500004fe
 -- IN
 SELECT id, name
 FROM departments
@@ -58,15 +78,3 @@ WHERE id IN (
   FROM sales
   WHERE price > 98
 );
-
--- EXISTS https://www.codewars.com/kata/58113a64e10b53ec36000293
-
--- CASE https://www.codewars.com/kata/593ef0e98b90525e090000b9/sql
-
--- POSITION https://www.codewars.com/kata/59401e0e54a655a298000040
-
--- JOIN and RANK https://www.codewars.com/kata/58094559c47d323ebd000035
-
--- UNION ALL https://www.codewars.com/kata/58112f8004adbbdb500004fe
-
--- HAVING https://www.codewars.com/kata/58164ddf890632ce00000220/solutions/sql
